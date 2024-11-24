279e31c84e994248559b430f6b689acb6f60ec12
AdityaNair07 <nairaditya07@gmail.com>
2024-11-24 20:52:50 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl font-semibold w-full text-center text-teal-500">
+      Role Based Access Control
+    </div>
+  );
+}
+
+export default App;
